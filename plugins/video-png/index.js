94dd61840e88867ea3a0b7852132374e1e7f5194
Max Degterev <me@maxdegterev.name>
2013-09-13 16:22:27 +0200

    sooper

--- plugins/video-png/index.js
+++ plugins/video-png/index.js
@@ -4,7 +4,6 @@ function video(name, deps) {
     var cv = require('opencv');
     var face_cascade = new cv.CascadeClassifier('node_modules/opencv/data/haarcascade_frontalface_alt2.xml');
     var processingImage = false;
-    var log = console.log;
     var fs = require('fs');
 
     // Add a new route to fetch camera image
@@ -31,24 +30,19 @@ function video(name, deps) {
         detectFacesOpenCv();
     });
 
-    if(latestImage) {
-    //setInterval(detectFacesCanvas, 150);
-    }
     var face_detect = require('face-detect');
+    var Canvas = require('canvas')
+    , Image = Canvas.Image
+    , canvas = new Canvas(640,360)
+    , ctx = canvas.getContext('2d');
+    , img = new Image
+    , troll = new Image;
 
-        var Canvas = require('canvas')
-        , Image = Canvas.Image
-        , canvas = new Canvas(640,360)
-        , ctx = canvas.getContext('2d');
-        img = new Image,
-        troll = new Image;
+    fs.readFile(__dirname + '/image.png', function(err, _troll){
+      if (err) throw err;
+      troll.src = _troll;
+    });
 
-        fs.readFile(__dirname + '/image.png', function(err, _troll){
-          if (err) throw err;
-          troll.src = _troll;
-        });
-
-    // facial foo
     var detectFacesOpenCv = function() {
         if ((!processingImage) && latestImage) {
             processingImage = true;
@@ -65,18 +59,15 @@ function video(name, deps) {
                     for (var k = 0; k < faces.length; k++) {
                         face = faces[k];
                         if (!biggestFace || biggestFace.width < face.width) biggestFace = face;
+                        console.warn(!biggestFace, biggestFace.width < face.width);
 
-                        im.rectangle([face.x, face.y], [face.x + face.width, face.y + face.height], [0, 255, 0], 2);
                         ctx.drawImage(troll, face.x, face.y, face.width, face.height);
                     }
 
-
                     latestImage = canvas.toBuffer();
-                    //latestImage = im.toBuffer();
                     processingImage = false;
 
                 }, opts.scale, opts.neighbors, opts.min && opts.min[0], opts.min && opts.min[1]);
-
             });
         };
     };
